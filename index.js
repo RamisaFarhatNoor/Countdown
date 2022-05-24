2157685e9ea83ff88ff229ea888788d84c1dfe36
Ramisa Noor <noor.ramisafarhat@gmail.com>
2022-05-24 21:35:40 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const daysEl=document.getElementById("days");
+const hoursEl=document.getElementById("hours");
+const minsEl=document.getElementById("mins");
+const secEl=document.getElementById("seconds");
+
+let given_date = "1 Jan 2023";
+
+function countdown(){
+    let new_date= new Date(given_date);
+    const current_date= new Date();
+    const totalsec=Math.floor(new_date-current_date)/1000;
+    const mins=Math.floor(totalsec/60) %24 % 60;
+    const hours=Math.floor(totalsec/3600)% 24;
+    const days= Math.floor(totalsec/3600/24);
+    const sec= Math.floor(totalsec)%60;
+    
+    daysEl.innerHTML = days;
+    hoursEl.innerHTML = format(hours);
+    minsEl.innerHTML = format(mins);
+    secEl.innerHTML = format(sec);
+
+
+
+
+    console.log(days,hours,mins,sec);
+
+
+
+}
+function format(time){
+
+    return time < 10 ? "0"+time : time; 
+}
+countdown();
+setInterval(countdown,1000);
